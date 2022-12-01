84518f0af10ad98837f3013d9e6609a441cb8357
A. Developers <ahmarjabbar7@gmail.com>
2022-12-02 01:41:26 +0500

    final

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
-const toggles = document.querySelectorAll(".drop-down-menu");
-const body = document.getElementById("bodys");
+// const toggles = document.querySelectorAll(".drop-down-menu");
+// const body = document.getElementById("bodys");
 
-function toggle(id) {
-  //close other dropdowns
-  for (var i = 0; i < toggles.length; i++) {
-    if (toggles[i].id === id) {
-      continue;
-    }
-    toggles[i].style.display = "none";
-  }
-  var element = document.getElementById(id);
-  if (element.style.display === "block") {
-    element.style.display = "none";
-    body.style.overflow = "auto";
-  } else {
-    element.style.display = "block";
-    body.style.overflow = "hidden";
-  }
-}
+// function toggle(id) {
+//   close other dropdowns
+//   for (var i = 0; i < toggles.length; i++) {
+//     if (toggles[i].id === id) {
+//       continue;
+//     }
+//     toggles[i].style.display = "none";
+//   }
+//   var element = document.getElementById(id);
+//   if (element.style.display === "block") {
+//     element.style.display = "none";
+//     body.style.overflow = "auto";
+//   } else {
+//     element.style.display = "block";
+//     body.style.overflow = "hidden";
+//   }
+// }
+let toggletop = () => {
+  let toggeleIt = document.getElementById("toggeleIt");
+  let down = document.getElementById("down");
+  let up = document.getElementById("up");
+  toggeleIt.classList.add("active");
+  toggeleIt.classList.remove("non-active");
+  down.style.display = "none";
+  up.style.display = "block";
+};
+let toggledown = () => {
+  let toggeleIt = document.getElementById("toggeleIt");
+  let down = document.getElementById("down");
+  let up = document.getElementById("up");
+  toggeleIt.classList.add("non-active");
+  toggeleIt.classList.remove("active");
+  down.style.display = "block";
+  up.style.display = "none";
+};
